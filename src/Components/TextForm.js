49e48fa17c6cec5f966bbc96f9052f64026294a5
ManthanRanpariya <ranpariyamanthan9930@gmail.com>
2023-03-25 20:44:05 +0530

    Add some fun in TextUtility

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -14,6 +14,15 @@ export default function TextForm(props) {
 		// console.log("On Change")
 		setText(event.target.value)
 	}
+	const handleCopy = () =>{
+		var text = document.getElementById("myBox")
+		text.select();
+		navigator.clipboard.writeText(text.value)
+	}
+	const handleExtraSpaces = ()=>{
+		let newText = text.split(/[ ]+/)
+		setText(newText.join(" "))
+	}
 	const [text, setText] = useState('Enter text here')
 	return (
 			<>
@@ -26,6 +35,8 @@ export default function TextForm(props) {
 			</div>
 			<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
 			<button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+			<button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+			<button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Space Remove</button>
 		</div>
 		<div className="container my-3">
 			<h1>Your text summary</h1>

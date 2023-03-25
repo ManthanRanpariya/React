aa39ca426fc0733dc12f090fd06920e1819e3a48
ManthanRanpariya <ranpariyamanthan9930@gmail.com>
2023-03-25 19:51:38 +0530

    About DarkMode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
-// import About from './Components/About';
+// import TextForm from './Components/TextForm';
+import About from './Components/About';
 function App() {
   return (
    <>
    <Navbar title="Textutils" aboutText="About us"/> 
    <div className="container my-3 ">
-   <TextForm heading="Enter the text to analyze"/>
-   {/* <About /> */}
+   {/* <TextForm heading="Enter the text to analyze"/> */}
+   <About />
    </div>
    </>
   );

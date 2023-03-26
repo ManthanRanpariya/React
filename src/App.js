ae394057c5c9083800a4239c04b846b522309b1e
ManthanRanpariya <ranpariyamanthan9930@gmail.com>
2023-03-26 12:54:12 +0530

    Dark-mode in TextUtils

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React,{useState} from 'react';
 // import About from './Components/About';
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = ()=>{
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+    }
+    else {
+      setMode('dark')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
    <>
-   <Navbar title="Textutils" aboutText="About us"/> 
+   <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/> 
    <div className="container my-3 ">
-   <TextForm heading="Enter the text to analyze"/>
+   <TextForm heading="Enter the text to Analyze below"  mode={mode}/>
    {/* <About /> */}
    </div>
    </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -26,11 +26,11 @@ export default function TextForm(props) {
 	const [text, setText] = useState('Enter text here')
 	return (
 			<>
-		<div className='Container'>
+		<div className='Container'style={{color:props.mode === 'dark'?'white':'#042743'}}>
 			<h1>{props.heading} </h1>
 			<div className="mb-3">
 
-				<textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+				<textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode === 'dark'?'grey':'white' , color:props.mode === 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
 
 			</div>
 			<button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
@@ -38,12 +38,12 @@ export default function TextForm(props) {
 			<button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
 			<button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Space Remove</button>
 		</div>
-		<div className="container my-3">
+		<div className="container my-3" style={{color:props.mode === 'dark'?'white':'#042743'}}>
 			<h1>Your text summary</h1>
 			<p>{text.split(" ").length} words and {text.length} characters</p>
 			<p>{0.008 * text.split(" ").length} Minutes read</p>
 			<h2>Privew</h2>
-			<p>{text}</p>
+			<p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
 		</div>
 			</>
 	)
